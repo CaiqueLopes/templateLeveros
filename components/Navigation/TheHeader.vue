afa9c91e7b2fc89abbf0cbc840b35f142b3820b9
Caique Lopes <caique.lopes@multiar.com.br>
2019-01-23 14:18:25 -0200

    Message in order

--- components/Navigation/TheHeader.vue
+++ components/Navigation/TheHeader.vue
@@ -22,11 +22,44 @@
         <v-btn slot="activator" dark icon>
           <v-icon>more_vert</v-icon>
         </v-btn>
-        <v-list>
-          <v-list-tile v-for="(info, inf) in infos" :key="inf" @ click>
-            <v-list-tile-title>{{info.dados}}</v-list-tile-title>
-          </v-list-tile>
-        </v-list>
+        <v-card>
+          <v-list>
+            <v-list-tile avatar>
+              <v-list-tile-avatar>
+                <img :src="imageLink" alt="user">
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>{{userName}}</v-list-tile-title>
+                <v-list-tile-sub-title>{{description}}</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+          <v-divider></v-divider>
+          <v-layout align-center row spacer>
+            <v-flex xs2 sm2 md2>
+              <v-avatar slot="activator">
+                <v-icon>person_outline</v-icon>
+              </v-avatar>
+            </v-flex>
+            <v-flex sm10 md10>
+              <span class="black--text">&nbsp;Perfil</span>
+            </v-flex>
+          </v-layout>
+          <v-layout align-center row spacer>
+            <v-flex xs2 sm2 md2>
+              <v-avatar slot="activator">
+                <v-icon>settings</v-icon>
+              </v-avatar>
+            </v-flex>
+            <v-flex sm10 md10>
+              <span class="black--text">&nbsp;Configurações</span>
+            </v-flex>
+          </v-layout>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="info" flat @click="menu = false">Logout</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-menu>
     </v-toolbar>
     <v-content>
@@ -48,12 +81,14 @@ export default {
         { icon: 'dashboard', title: 'Home', to: '/' },
         { icon: 'edit', title: 'Pedidos', to: '/pedidos' }
       ],
-      infos: [
-        { dados: 'Click Me' },
-        { dados: 'Click Me' },
-        { dados: 'Click Me' },
-        { dados: 'Click Me' }
-      ],
+      description: 'Descrição do usuário',
+      userName: 'Nome do usuário',
+      imageLink:
+        'http://icons.iconarchive.com/icons/aha-soft/standard-new-year/128/Snowflake-icon.png',
+      fav: true,
+      menu: false,
+      message: false,
+      hints: true,
       miniVariant: false,
       logo: 'Nuxtfy.js'
     }
--- pages/pedidos/index.vue
+++ pages/pedidos/index.vue
@@ -2,6 +2,7 @@
   <v-card>
     <header>
       <v-card-title>
+        <!--AJUSTAR COM A CSS TAG CORRETA>>>>> <v-flex tag="h1" class="headline">Exemplo</v-flex> -->
         <h1 style="color:#4a99ca;">Pedidos</h1>
       </v-card-title>
       <v-container fluid grid-list-xl>
@@ -41,14 +42,64 @@
             <td class="text-xs-right">{{ props.item.iron }}</td>
           </tr>
         </template>
-
         <template slot="expand" slot-scope="props">
           <v-card flat>
-            <v-card-text>Teste</v-card-text>
+            <v-card-text>
+              <div v-for="item in dados" :key="item.title">
+                <v-layout row>
+                  <v-flex>
+                    <v-menu
+                      v-model="menu"
+                      :close-on-content-click="false"
+                      :nudge-width="100"
+                      offset-x
+                    >
+                      <v-btn slot="activator" color="info" round class="white--text">mostrar mais</v-btn>
+                      <v-card>
+                        <v-layout align-center row spacer>
+                          <v-flex xs2 sm2 md2>
+                            <v-avatar slot="activator">
+                              <v-icon>autorenew</v-icon>
+                            </v-avatar>
+                          </v-flex>
+                          <v-flex sm10 md10>
+                            <span class="black--text">&nbsp; Reprocessar</span>
+                          </v-flex>
+                        </v-layout>
+                        <v-layout align-center row spacer>
+                          <v-flex xs2 sm2 md2>
+                            <v-avatar slot="activator">
+                              <v-icon>autorenew</v-icon>
+                            </v-avatar>
+                          </v-flex>
+                          <v-flex sm10 md10>
+                            <span class="black--text">&nbsp; Ignorar divergência e reprocessar</span>
+                          </v-flex>
+                        </v-layout>
+                      </v-card>
+                    </v-menu>
+                  </v-flex>
+                  <v-flex>
+                    <v-card-text>
+                      <a href="#">{{ item.order }}</a>
+                    </v-card-text>
+                  </v-flex>
+                  <v-flex>
+                    <v-card-text>{{ item.status }}</v-card-text>
+                  </v-flex>
+                  <v-flex>
+                    <v-card-text>{{ item.lastUpdate }}</v-card-text>
+                  </v-flex>
+                  <v-flex justify-end>
+                    <v-icon v-html="item.ico"></v-icon>
+                  </v-flex>
+                </v-layout>
+              </div>
+            </v-card-text>
           </v-card>
         </template>
         <v-alert slot="no-results" :value="true" color="error" icon="warning">
-          Nenhum resultado encontrado para "{{ search }}".
+          Nenhum resultado encontrado para "{{search}}".
           <br>:(
         </v-alert>
       </v-data-table>
@@ -172,21 +223,35 @@ export default {
           protein: 7,
           iron: '6%'
         }
+      ],
+      dados: [
+        {
+          ico: 'check_circle_outline',
+          order: 'PEDIDO 0000-00',
+          buyer: 'E-MAIL DO COMPRADOR',
+          status: 'Processo com sucesso',
+          lastUpdate: 'Data do ultimo processamento'
+        } /*,
+        {
+          ico: 'error_outline',
+          order: 'PEDIDO 1111-00',
+          buyer: 'E-MAIL DO COMPRADOR',
+          status: 'Erro',
+          lastUpdate: 'Data do ultimo processamento'
+        }*/
       ]
     }
   },
   computed: {
-        pages() {
-          if (
-            this.pagination.rowsPerPage == null ||
-            this.pagination.totalItems == null
-          )
-            return 0
+    pages() {
+      if (
+        this.pagination.rowsPerPage == null ||
+        this.pagination.totalItems == null
+      )
+        return 0
 
-          return Math.ceil(
-            this.pagination.totalItems / this.pagination.rowsPerPage
-          )
-        }
-      }
+      return Math.ceil(this.pagination.totalItems / this.pagination.rowsPerPage)
+    }
+  }
 }
 </script>
